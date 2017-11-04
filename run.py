790a8f63e82d43d3e8fae53832c48a22b1518cc4
张昌盛 <zcs@192.168.0.103>
2017-11-04 18:44:42 +0800

    add format scrapy

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 # File Name: run.py
 # Author: Changsheng Zhang
 # mail: zhangcsxx@gmail.com
@@ -18,23 +19,45 @@ def format_data():
         lines = open("holding_data/"+name).readlines()
         for ii in xrange(1,len(lines)):
             line = lines[ii].split(";")
-            code = int(line[0])
-            f = open("format_data/"+str(code)+".csv","a")
+            code = str(int(line[0]))
+            for kk in xrange(4-len(code)):
+                code = "0"+code
+
+            f = open("format_data/"+code+".csv","a")
             tmp = line[2].split(",")
             stock_num = ""
             for ii in tmp:
                 stock_num = stock_num + ii
             
             if flag == 0:
-                f.write("date;holding_num;holding_perc\n")
+                f.write("date,holding_num,holding_perc\n")
 
-            f.write(name.split(".")[0]+";"+stock_num+";"+line[3]+"\n")
+            f.write(name.split(".")[0]+","+stock_num.strip(" ")+","+line[3].strip(" "))
 
         flag = 1
 
-def merge(kk,index):
+def merged(kk,index):
 
-    print 1
+    filename = sorted(os.listdir("format_data/"))
+
+    output_lines = []
+    header = "stock_id"
+    flag = 0
+    for stock_num in filename:
+
+        output = stock_num.split('.')[0]
+        lines = open("format_data/"+stock_num).readlines()
+        for ii in xrange(1,len(lines)):
+            line = lines[ii].split(",")
+            if flag == 0:
+                header = header + "," + line[0]
+            output = output +"," +line[kk].strip("\n")
+        output_lines.append(output+"\n")
+        flag = 1
+    f = open("output/"+index+".csv","w")
+    f.write(header+"\n")
+    f.writelines(output_lines)
+
 
 for ii in xrange(1000):
 
@@ -44,6 +67,9 @@ for ii in xrange(1000):
     time.sleep(10)
     os.system("rm -rf format_data/*")
     format_data()
+    time.sleep(10)
+    merged(1,"hold_num")
+    merged(2,"hold_perc")
     print "done, begin sleep"
     a = 3600*24-10
     time.sleep(a)
