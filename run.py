1d8889f0c0be39fa1fc1c747b1ea18a33d63ab51
张昌盛 <zcs@192.168.0.103>
2017-11-25 15:26:35 +0800

    fix merge py

--- run.py
+++ run.py
@@ -8,6 +8,7 @@
 
 import os
 import time
+import sys
 
 def format_data():
 
@@ -61,6 +62,40 @@ def merged(kk,index):
     f.writelines(output_lines)
 
 
+def final_merged():
+
+    output_num_lines = []
+    output_perc_lines = []
+    for file_name in sorted(os.listdir("format_data")):
+
+        stock_id = file_name[0:4]+".hk"
+        content = open("format_data/"+file_name).readlines()
+        date_line = "date"
+        stock_num_line = stock_id 
+        stock_perc_line = stock_id
+        for ii in xrange(1,len(content)):
+            tmp_line = content[ii].strip("\n").split(",")
+            date_line = date_line+","+tmp_line[0]
+            stock_num_line = stock_num_line+","+tmp_line[1]
+            stock_perc_line = stock_perc_line +"," +tmp_line[2]
+
+        if len(output_num_lines) == 0:
+            output_num_lines.append(date_line+"\n")
+        if len(output_perc_lines) == 0:
+            output_perc_lines.append(date_line+"\n")
+
+        output_num_lines.append(stock_num_line +"\n")
+        output_perc_lines.append( stock_perc_line + "\n")
+
+    f = open("output_data/stock_num.csv","w")
+    f.writelines(output_num_lines)
+    f.close()
+
+    f = open("output_data/stock_perc.csv","w")
+    f.writelines(output_perc_lines)
+    f.close()
+
+
 for ii in xrange(1000):
 
     print "begin run\n"
@@ -72,6 +107,10 @@ for ii in xrange(1000):
     time.sleep(10)
     merged(1,"hold_num")
     merged(2,"hold_perc")
+
+    time.sleep(10)
+    final_merged()
+
     print "done, begin sleep"
-    a = 3600*24-20
+    a = 3600*24-30
     time.sleep(a)
